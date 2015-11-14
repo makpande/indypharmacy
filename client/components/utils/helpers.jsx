67cd3aabc2efd3df8a70830b99fe017c8827d74c
dpg5000 <dgoussev@gmail.com>
2015-11-14 10:49:00 -0800

    keys.jsx

--- client/components/utils/helpers.jsx
+++ client/components/utils/helpers.jsx
@@ -26,4 +26,5 @@ module.exports = helpers;
 
 
 // You're exporting an object with properties called API_KEY and PRIV_KEY:
-// So dance your pants off
+// So dance your pants off to
+// A Certain Ratio
