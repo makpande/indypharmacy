97558f163b77e37c4c644f37b33ab706427064cd
dpg5000 <dgoussev@gmail.com>
2015-11-15 21:55:38 -0800

    remove node_env in config.js

--- webpack.pro.config.js
+++ webpack.pro.config.js
@@ -1,13 +1,13 @@
 var config = require('./webpack.config.js');
 var webpack = require('webpack');
 
-config.plugins.push(
-  new webpack.DefinePlugin({
-    "process.env": {
-      "NODE_ENV": JSON.stringify("production")
-    }
-  })
-);
+// config.plugins.push(
+//   new webpack.DefinePlugin({
+//     "process.env": {
+//       "NODE_ENV": JSON.stringify("production")
+//     }
+//   })
+// );
 
 config.plugins.push(
   new webpack.optimize.UglifyJsPlugin({
