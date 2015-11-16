8ea4834999e4ee3b6705fa51ae74b8a190e926cd
dpg5000 <dgoussev@gmail.com>
2015-11-15 22:17:03 -0800

    stringify keys

--- webpack.pro.config.js
+++ webpack.pro.config.js
@@ -8,7 +8,8 @@ config.plugins.push(
   new webpack.DefinePlugin({
     "process.env": {
       "NODE_ENV": JSON.stringify("production"),
-      "API_KEY" : process.env["API_KEY"]
+      "API_KEY" : JSON.stringify(process.env["API_KEY"]),
+      "PRIV_KEY" : JSON.stringify(process.env["PRIV_KEY"])
     }
   })
 );
